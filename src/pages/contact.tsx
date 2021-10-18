eba8d7200f8b8ae0ed94558a0652f2781b85206c
mojao <rocker_metal0227@icloud.com>
2021-10-18 15:20:41 +0900

    return

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -44,6 +44,7 @@ export default function Contact() {
         <h1 style={{ marginTop: 0 }}>Contact</h1>
 
         <div className={contactStyles.name}>
+          JJ Kasper - Next.js Engineer @{' '}
           <ExtLink href="https://vercel.com">Vercel</ExtLink>
         </div>
 
